a4a4d146d4abdf04d91a4c972a89ff78f7644cd2
surajboss44 <suraj.boss44@gmail.com>
2014-09-30 14:51:15 -0500

    Update exercise3.py
    The initial logic for computing the outcome of a Rock, Paper and Scissors game between two participants has been completed. 
    
    Checking in the Initial Code Version

--- exercise3.py
+++ exercise3.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 
-def decide_rps(player1, player2):
-    return 1
+def RpsGame(player1, player2):
+    Dictionary1 = {'rock paper': 2, 'rock scissors': 1, 'paper rock': 1, 'paper scissors': 2, 'scissors paper': 1,
+                   'scissors rock': 2}
+    ComputeValue = player1+' '+player2
+    print(ComputeValue)
+    for indexValue in Dictionary1:
+        if indexValue == ComputeValue: #print(Dictionary1[input])
+            return Dictionary1[ComputeValue]
+    else:
+        return 0
+
+print(RpsGame('rock','scissors'))
 
