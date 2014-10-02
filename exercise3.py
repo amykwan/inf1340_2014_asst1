d896c68fb0dd5289a76f3e674f65e016cb0b9769
surajboss44 <suraj.boss44@gmail.com>
2014-10-02 18:32:55 -0400

    Code modified. Implemented Dictionary concept to the code.

--- exercise3.py
+++ exercise3.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 
-def RpsGame(player1, player2):
-    Dictionary1 = {'rock paper': 2, 'rock scissors': 1, 'paper rock': 1, 'paper scissors': 2, 'scissors paper': 1,
-                   'scissors rock': 2}
+def decide_rps(player1, player2):
+    Dictionary1 = {'Rock Paper': 2, 'Rock Scissors': 1, 'Paper Rock': 1, 'Paper Scissors': 2, 'Scissors Paper': 1,
+                   'Scissors Rock': 2, 'Rock Rock': 0, 'Paper Paper': 0, 'Scissors Scissors': 0}
     ComputeValue = player1+' '+player2
     print(ComputeValue)
     for indexValue in Dictionary1:
-        if indexValue == ComputeValue: #print(Dictionary1[input])
+        if indexValue == ComputeValue:
             return Dictionary1[ComputeValue]
     else:
-        return 0
+        return "Invalid Input"
 
-print(RpsGame('rock','scissors'))
+print(decide_rps('Rock', 'Pen'))
 
