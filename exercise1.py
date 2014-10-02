a91ef7a1092726f2503d1362d2a649ba35cc21e5
surajboss44 <suraj.boss44@gmail.com>
2014-10-02 11:13:58 -0400

    Code modified. Implemented Dictionary concept to the code.

--- exercise1.py
+++ exercise1.py
@@ -23,6 +23,7 @@ __status__ = "Prototype"
 
 
 def grade_to_gpa(grade):
+
     """
     Returns the UofT Graduate GPA for a given grade.
 
@@ -39,29 +40,46 @@ def grade_to_gpa(grade):
         TypeError if parameter is not a string or integer
         ValueError if parameter is out of range
     """
-
-    letter_grade = ""
-    gpa = 0.0
-
+    #letter_grade = ""
+    #gpa = 0.0
+    #Create a Dictionary to store the Grades and their equivalent GPAs
+    Dictionary1 = {'A+':4.0, 'A':4.0, 'A-':3.7, 'B+': 3.3, 'B':3.0, 'B-':2.7, 'FZ':0.0}
+    #Define a function to convert the Grade which is of String Data type to GPA
+    def StringToGPA(grade1):
+        for indexValue in Dictionary1:
+            if indexValue == grade1:
+                return Dictionary1[grade1]
     if type(grade) is str:
-        print ("letter") # remove this line once the code is implemented
-        # check that the grade is one of the accepted values
-        # assign grade to letter_grade
+        gpa = StringToGPA(grade)
     elif type(grade) is int:
-        print("mark") # remove this line once the code is implemented
+        print("mark")
+        if 90<=grade<=100:
+            letter_grade = "A+"
+        elif 85<=grade<=89:
+            letter_grade = "A"
+        elif 80<=grade<=84:
+            letter_grade = "A-"
+        elif 77<=grade<=79:
+            letter_grade = "B+"
+        elif 73<=grade<=76:
+            letter_grade = "B"
+        elif 70<=grade<=72:
+            letter_grade = "B-"
+        elif 0<=grade<=69:
+            letter_grade = "FZ"
+        gpa = StringToGPA(letter_grade)                 # remove this line once the code is implemented
         # check that grade is in the accepted range
         # convert the numeric grade to a letter grade
         # assign the value to letter_grade
         # hint: letter_grade = mark_to_letter(grade)
     else:
-        # raise a TypeError exception
+            # raise a TypeError exception
         print ("error")
         raise TypeError("Invalid type passed as parameter")
-
     # write a long if-statement to convert letter_grade
     # assign the value to gpa
-    if letter_grade == "A":
-        gpa = 4.0
-
+    #if letter_grade == "A":
+        #gpa = 4.0
     return gpa
 
+print(grade_to_gpa(77))
