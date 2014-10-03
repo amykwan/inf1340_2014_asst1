7367065c7b00b5c7d5d03fd8f432d6807beaa188
amykwan <amykwan.cma@gmail.com>
2014-10-03 17:16:54 -0400

    Merge branch 'master' of https://github.com/amykwan/inf1340_2014_asst1
    Conflicts:
    	exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion
+""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion.
 
 This module contains one function grade_to_gpa. It can be passed a parameter
 that is an integer (0-100) or a letter grade (A+, A, A-, B+, B, B-, or FZ). All
@@ -11,7 +9,7 @@ Example:
 
 """
 
-__author__ = 'Susan Sim'
+__author__ = 'Amy Kwan, Suraj Narayanan, Sue Min'
 __email__ = "ses@drsusansim.org"
 
 __copyright__ = "2014 Susan Sim"
@@ -23,7 +21,6 @@ __status__ = "Prototype"
 
 
 def grade_to_gpa(grade):
-
     """
     Returns the UofT Graduate GPA for a given grade.
 
@@ -40,46 +37,61 @@ def grade_to_gpa(grade):
         TypeError if parameter is not a string or integer
         ValueError if parameter is out of range
     """
-    #letter_grade = ""
-    #gpa = 0.0
-    #Create a Dictionary to store the Grades and their equivalent GPAs
-    Dictionary1 = {'A+':4.0, 'A':4.0, 'A-':3.7, 'B+': 3.3, 'B':3.0, 'B-':2.7, 'FZ':0.0}
-    #Define a function to convert the Grade which is of String Data type to GPA
-    def StringToGPA(grade1):
-        for indexValue in Dictionary1:
-            if indexValue == grade1:
-                return Dictionary1[grade1]
+
+    letter_grade = ""
+    gpa = 0.0
+
     if type(grade) is str:
-        gpa = StringToGPA(grade)
+        #print ("letter") # remove this line once the code is implemented
+        # check that the grade is one of the accepted values
+        if grade == "A+" or grade == "A" or grade == "A-" or grade == "B+" or grade == "B" or grade == "B-" or grade == "FZ":
+            # assign grade to letter_grade
+            letter_grade = grade
+        else:
+            print ("error")
+            raise ValueError ("Invalid value passed as parameter")
+
     elif type(grade) is int:
-        print("mark")
-        if 90<=grade<=100:
-            letter_grade = "A+"
-        elif 85<=grade<=89:
-            letter_grade = "A"
-        elif 80<=grade<=84:
-            letter_grade = "A-"
-        elif 77<=grade<=79:
-            letter_grade = "B+"
-        elif 73<=grade<=76:
-            letter_grade = "B"
-        elif 70<=grade<=72:
-            letter_grade = "B-"
-        elif 0<=grade<=69:
-            letter_grade = "FZ"
-        gpa = StringToGPA(letter_grade)                 # remove this line once the code is implemented
+        #print("mark") # remove this line once the code is implemented
         # check that grade is in the accepted range
-        # convert the numeric grade to a letter grade
-        # assign the value to letter_grade
-        # hint: letter_grade = mark_to_letter(grade)
+        if grade >= 0 and grade <= 100:
+            # convert the numeric grade to a letter grade
+                # assign the value to letter_grade
+                # hint: letter_grade = mark_to_letter(grade)
+            if grade >= 90:
+                letter_grade = "A+"
+            elif grade >= 85:
+                letter_grade = "A"
+            elif grade >= 80:
+                letter_grade = "A-"
+            elif grade >= 77:
+                letter_grade = "B+"
+            elif grade >= 73:
+                letter_grade = "B"
+            elif grade >= 70:
+                letter_grade = "B-"
+            elif grade >= 0:
+                letter_grade = "FZ"
+        else:
+            print ("error")
+            raise ValueError ("Grade out of range")
     else:
-            # raise a TypeError exception
+        # raise a TypeError exception
         print ("error")
         raise TypeError("Invalid type passed as parameter")
+
     # write a long if-statement to convert letter_grade
     # assign the value to gpa
-    #if letter_grade == "A":
-        #gpa = 4.0
+    if letter_grade == "A+" or letter_grade == "A":
+        gpa = 4.0
+    elif letter_grade == "A-":
+        gpa = 3.7
+    elif letter_grade == "B+":
+        gpa = 3.3
+    elif letter_grade == "B":
+        gpa = 3.0
+    elif letter_grade == "B-":
+        gpa = 2.7
+    elif letter_grade == "FZ":
+        gpa = 0.0
     return gpa
-
-print(grade_to_gpa(77))
